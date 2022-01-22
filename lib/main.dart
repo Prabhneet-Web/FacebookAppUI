e4305384cf5094484ef1464a862019da5b20d42d
Prabhneet Narula <imailcode@gmail.com>
2022-01-22 22:43:58 +0530

    Some changes done on the HomePage.

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage()
-    );
+        debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -16,10 +16,72 @@ class _HomePageState extends State<HomePage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
+            padding:
+                const EdgeInsets.only(top: 50, bottom: 10, right: 20, left: 20),
             height: 120,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.grey[200]),
+                    child: const TextField(
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.search, color: Colors.grey),
+                            border: InputBorder.none,
+                            hintStyle: TextStyle(color: Colors.grey),
+                            hintText: "Search")),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                Icon(Icons.camera_alt, color: Colors.grey[800], size: 30)
+              ],
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: [
+                        Text(
+                          "Stories",
+                          style: TextStyle(
+                              color: Colors.grey[900],
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22,
+                              letterSpacing: 1.1),
+                        ),
+                        Text("See Archive")
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      height: 180,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [makeStory()],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           )
         ],
       ),
     );
+  }
+
+  Widget makeStory() {
+    return AspectRatio(aspectRatio: 1 / 3);
   }
 }
