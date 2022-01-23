8afd2fa4fc0df11f04ebc76e1f7f69a6a140f398
Prabhneet Narula <imailcode@gmail.com>
2022-01-23 12:11:44 +0530

    AppBar added.

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -12,102 +12,115 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding:
-                const EdgeInsets.only(top: 50, bottom: 10, right: 20, left: 20),
-            height: 120,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.grey[200]),
-                    child: const TextField(
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.search, color: Colors.grey),
-                            border: InputBorder.none,
-                            hintStyle: TextStyle(color: Colors.grey),
-                            hintText: "Search")),
-                  ),
-                ),
-                const SizedBox(width: 20),
-                Icon(Icons.camera_alt, color: Colors.grey[800], size: 30)
-              ],
-            ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.baseline,
-                      textBaseline: TextBaseline.alphabetic,
-                      children: [
-                        Text(
-                          "Stories",
-                          style: TextStyle(
-                              color: Colors.grey[900],
-                              fontWeight: FontWeight.bold,
-                              fontSize: 22,
-                              letterSpacing: 1.1),
-                        ),
-                        Text("See Archive")
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    Container(
-                      height: 180,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: [
-                          makeStory(
-                              storyImage: "lib/assets/images/story/Story1.webp",
-                              userImage:
-                                  "lib/assets/images/profilePics/Profile1.jpeg",
-                              userName: "Alok Bhandari"),
-                          makeStory(
-                              storyImage: "lib/assets/images/story/Story2.webp",
-                              userImage:
-                                  "lib/assets/images/profilePics/Profile2.jpeg",
-                              userName: "Mukul Giri"),
-                          makeStory(
-                              storyImage: "lib/assets/images/story/Story3.webp",
-                              userImage:
-                                  "lib/assets/images/profilePics/Profile3.jpeg",
-                              userName: "Ujjwal Agrawal"),
-                          makeStory(
-                              storyImage: "lib/assets/images/story/Story4.webp",
-                              userImage:
-                                  "lib/assets/images/profilePics/Profile4.jpeg",
-                              userName: "Priyanshu Pokhriyal"),
-                          makeStory(
-                              storyImage: "lib/assets/images/story/Story5.webp",
-                              userImage:
-                                  "lib/assets/images/profilePics/Profile5.jpeg",
-                              userName: "Sagar Mamgain"),
-                          makeStory(
-                              storyImage: "lib/assets/images/story/Story6.webp",
-                              userImage:
-                                  "lib/assets/images/profilePics/Profile6.jpeg",
-                              userName: "Darshan Raval")
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 20, top: 20),
+              child: const Text(
+                "faceboook",
+                style: TextStyle(
+                    fontSize: 37,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w800),
               ),
             ),
-          )
-        ],
+            Container(
+              padding:
+                  const EdgeInsets.only(top: 16, bottom: 10, right: 20, left: 20),
+              height: 60,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.grey[200]),
+                      child: const TextField(
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.search, color: Colors.grey),
+                              border: InputBorder.none,
+                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: "Search")),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Icon(Icons.camera_alt_outlined,
+                      color: Colors.grey[800], size: 30)
+                ],
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text(
+                            "Stories",
+                            style: TextStyle(
+                                color: Colors.grey[900],
+                                fontWeight: FontWeight.w900,
+                                fontSize: 24,
+                                letterSpacing: 1.0),
+                          ),
+                          Text("See Archive")
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Container(
+                        height: 180,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            makeStory(
+                                storyImage: "lib/assets/images/story/Story1.webp",
+                                userImage:
+                                    "lib/assets/images/profilePics/Profile1.jpeg",
+                                userName: "Alok Bhandari"),
+                            makeStory(
+                                storyImage: "lib/assets/images/story/Story2.webp",
+                                userImage:
+                                    "lib/assets/images/profilePics/Profile2.jpeg",
+                                userName: "Mukul Giri"),
+                            makeStory(
+                                storyImage: "lib/assets/images/story/Story3.webp",
+                                userImage:
+                                    "lib/assets/images/profilePics/Profile3.jpeg",
+                                userName: "Ujjwal Agrawal"),
+                            makeStory(
+                                storyImage: "lib/assets/images/story/Story4.webp",
+                                userImage:
+                                    "lib/assets/images/profilePics/Profile4.jpeg",
+                                userName: "Priyanshu Pokhriyal"),
+                            makeStory(
+                                storyImage: "lib/assets/images/story/Story5.webp",
+                                userImage:
+                                    "lib/assets/images/profilePics/Profile5.jpeg",
+                                userName: "Sagar Mamgain"),
+                            makeStory(
+                                storyImage: "lib/assets/images/story/Story6.webp",
+                                userImage:
+                                    "lib/assets/images/profilePics/Profile6.jpeg",
+                                userName: "Darshan Raval")
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
